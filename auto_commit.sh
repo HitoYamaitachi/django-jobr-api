09a5e0b8dcab69e188e07e1f6777d77d4862bd59
hito0559 <hitoyamaitachi@gmail.com>
2021-12-07 22:13:13 +0200

    init commit

---
+++ auto_commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set the start date (3 years ago)
+start_date=$(date -d "3 years ago" +%Y-%m-%d)
+# Number of commits (2 months = approximately 60 days, every 3 days)
+num_commits=$((60 / 3))
+
+# Loop to create commits
+for ((i=0; i<num_commits; i++))
+do
+    # Calculate the date for the commit
+    commit_date=$(date -d "$start_date + $((i * 3)) days" +%Y-%m-%d)
+
+    # Create a new file or modify an existing file
+    echo "Commit for date $commit_date" > "file_$i.txt"
+    
+    # Stage the file
+    git add "file_$i.txt"
+    
+    # Commit with the calculated date
+    GIT_COMMITTER_DATE="$commit_date 12:00:00" git commit -m "Auto-commit for $commit_date" --date "$commit_date 12:00:00"
+done
+
+echo "Commits created successfully!"
+ 
