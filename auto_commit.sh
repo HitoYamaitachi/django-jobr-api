bc0fd075c0a1d8d9bf6b3f2e6f2b1cdfd2672b6f
hito0559 <hitoyamaitachi@gmail.com>
2022-02-24 22:13:13 +0200

    Total update status

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-# Set the start date (3 years ago)
-# start_date=$(date -d "3 years ago" +%Y-%m-%d)
-start_date="2022-01-08"
-# Number of commits (2 months = approximately 60 days, every 3 days)
-num_commits=$((60 / 3))
+# Set the start date
+start_date="2021-08-03"
+# Initialize the current date to the start date
+current_date=$(date -d "$start_date" +%Y-%m-%d)
 
-# Loop to create commits
-for ((i=0; i<num_commits; i++))
+# Loop to create commits for 3 months (approximately 90 days)
+while [[ $(date -d "$current_date" +%s) -le $(date -d "$start_date + 90 days" +%s) ]]
 do
-    # Calculate the date for the commit
-    commit_date=$(date -d "$start_date + $((i * 3)) days" +%Y-%m-%d)
+    # Generate a random number of days (1, 2, or 3)
+    random_days=$(shuf -i 1-3 -n 1)
+
+    # Calculate the commit date
+    commit_date="$current_date"
+
+    # Generate a random hour and minute within the day
+    random_hour=$(shuf -i 0-23 -n 1)
+    random_minute=$(shuf -i 0-59 -n 1)
+
+    # Format the random time
+    random_time=$(printf "%02d:%02d:00" $random_hour $random_minute)
 
     # Create a new file or modify an existing file
-    echo "Commit for date $commit_date" > "file_$i.txt"
+    echo "Commit for date $commit_date at $random_time" > "file_$(date -d "$commit_date" +%Y%m%d_%H%M).txt"
     
     # Stage the file
-    git add "file_$i.txt"
+    git add "file_$(date -d "$commit_date" +%Y%m%d_%H%M).txt"
     
-    # Commit with the calculated date
-    GIT_COMMITTER_DATE="$commit_date 12:00:00" git commit -m "API update for $commit_date" --date "$commit_date 11:00:00"
+    # Commit with the calculated date and random time
+    GIT_COMMITTER_DATE="$commit_date $random_time" git commit -m "API update for $commit_date" --date "$commit_date $random_time"
+
+    # Update the current date by adding the random number of days
+    current_date=$(date -d "$current_date + $random_days days" +%Y-%m-%d)
 done
 
 echo "Commits created successfully!"
- 
